7a6ec6f74a40184daf98b28f0ff3ecf827e95c33
Thomas <zgsoft_happy@126.com>
2017-10-14 22:12:38 +0800

    放在本地库

---
+++ Spring/思维导图/Spring beans配置.mm
@@ -0,0 +1,145 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Spring beans&#x914d;&#x7f6e;" FOLDED="false" ID="ID_1754415100" CREATED="1507650692858" MODIFIED="1507651161780" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="BeanFactory" POSITION="right" ID="ID_473849523" CREATED="1507650761053" MODIFIED="1507989284238">
+<edge COLOR="#0000ff"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#36825;&#26159;&#22791;&#27880;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x4e0a;&#x4e0b;&#x6587;&#x914d;&#x7f6e;ApplicationContext" POSITION="right" ID="ID_767641079" CREATED="1507650772774" MODIFIED="1507989294819">
+<icon BUILTIN="bookmark"/>
+<edge COLOR="#00ff00"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#36825;&#20010;&#20063;&#26159;&#22791;&#27880;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="ClassPathXmlApplicationContext" ID="ID_572082021" CREATED="1507650852723" MODIFIED="1507989260414">
+<icon BUILTIN="full-1"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ApplicationContext ctx = new ClassPathXmlApplicationContext(&quot;*.xml&quot;);
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="FileSystemXmlApplicationContext" ID="ID_1020399210" CREATED="1507650868673" MODIFIED="1507989301428">
+<icon BUILTIN="full-2"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#22791;&#27880;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="AnnotationConfigApplicationContext" ID="ID_956958508" CREATED="1507650791115" MODIFIED="1507650987635">
+<icon BUILTIN="full-3"/>
+</node>
+<node TEXT="XmlWebApplicationContext" ID="ID_1433549470" CREATED="1507650888164" MODIFIED="1507650994548">
+<icon BUILTIN="full-4"/>
+</node>
+<node TEXT="AnnotationConfigWebApplicationContext" ID="ID_1740239941" CREATED="1507650832120" MODIFIED="1507650997911">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+</node>
+</map>
