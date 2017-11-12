c35806f560ba588d773a820888e23d5558b89360
Thomas <zgsoft_happy@126.com>
2017-11-12 17:41:55 +0800

    spring高级配置

--- Spring/思维导图/Spring配置.mm
+++ Spring/思维导图/Spring配置.mm
@@ -63,7 +63,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="12" RULE="ON_BRANCH_CREATION"/>
 <richcontent TYPE="DETAILS">
 
 <html>
@@ -91,7 +91,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="constructor-arg" ID="ID_1803764945" CREATED="1509287111642" MODIFIED="1509287648792"><richcontent TYPE="DETAILS" HIDDEN="true">
 
@@ -111,7 +110,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="c&#x547d;&#x540d;&#x7a7a;&#x95f4;" ID="ID_967487526" CREATED="1509287234217" MODIFIED="1509287644876"><richcontent TYPE="DETAILS" HIDDEN="true">
@@ -138,7 +136,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -153,7 +150,6 @@
   <body>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="property" ID="ID_1178888260" CREATED="1509287655386" MODIFIED="1509287735076"><richcontent TYPE="DETAILS" HIDDEN="true">
 
@@ -167,7 +163,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="p&#x547d;&#x540d;&#x7a7a;&#x95f4;" ID="ID_1681115880" CREATED="1509287737272" MODIFIED="1509287964080"><richcontent TYPE="DETAILS" HIDDEN="true">
@@ -188,7 +183,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="util" ID="ID_1509370449" CREATED="1509287855371" MODIFIED="1509287957806"><richcontent TYPE="DETAILS" HIDDEN="true">
 
@@ -205,7 +199,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -225,7 +218,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="@Bean" ID="ID_1985891170" CREATED="1509288001194" MODIFIED="1509288018296"><richcontent TYPE="DETAILS">
@@ -240,7 +232,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -258,7 +249,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="&#x7ec4;&#x4ef6;&#x626b;&#x63cf;" ID="ID_1451156474" CREATED="1508074433067" MODIFIED="1508074443521">
 <node TEXT="@Component" ID="ID_591594749" CREATED="1508074488631" MODIFIED="1508074499566"/>
@@ -326,7 +316,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="xml&#x6df7;&#x5408;&#x5176;&#x4ed6;" FOLDED="true" ID="ID_318874390" CREATED="1509288206575" MODIFIED="1509288215798">
 <node TEXT="`&lt;import resource=&quot;&quot;/&gt;`" ID="ID_905004114" CREATED="1509288376044" MODIFIED="1509288441295"><richcontent TYPE="DETAILS">
@@ -341,7 +330,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="`&lt;bean class=&quot;&quot;/&gt;`" ID="ID_493725534" CREATED="1509288441932" MODIFIED="1509288522249"><richcontent TYPE="DETAILS">
@@ -356,7 +344,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="`&lt;context:component-scan&gt;`" ID="ID_349509664" CREATED="1509288473961" MODIFIED="1509288516448"><richcontent TYPE="DETAILS">
@@ -371,7 +358,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -388,7 +374,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="@ImportResource" ID="ID_292657886" CREATED="1509288267170" MODIFIED="1509288314501"><richcontent TYPE="DETAILS">
@@ -403,7 +388,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="@ComponentScan" ID="ID_1868025330" CREATED="1509288340527" MODIFIED="1509288371930"><richcontent TYPE="DETAILS">
@@ -418,8 +402,274 @@
     </p>
   </body>
 </html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="&#x9ad8;&#x7ea7;&#x88c5;&#x914d;" POSITION="left" ID="ID_1925182395" CREATED="1509494806148" MODIFIED="1509494820850">
+<edge COLOR="#007c00"/>
+<node TEXT="&#x73af;&#x5883;&#x4e0e;profile" ID="ID_937713310" CREATED="1509494857064" MODIFIED="1509494898437">
+<node TEXT="&#x914d;&#x7f6e;profile bean" ID="ID_269356394" CREATED="1509494901630" MODIFIED="1509494929291">
+<node TEXT="@Profile" ID="ID_1828280702" CREATED="1509494953085" MODIFIED="1509495086184"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      @Profile(&quot;dev&quot;)
+    </p>
+    <p>
+      &#21487;&#22312;&#37197;&#32622;&#31867;&#19978;&#22768;&#26126;&#65292;&#20063;&#21487;&#22312;&#36820;&#22238;bean&#30340;&#26041;&#27861;&#19978;&#65288;spring3.2&#65289;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="`profile=&quot;dev&quot;" ID="ID_1894782469" CREATED="1509494982325" MODIFIED="1509495131720"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#29992;&#22312;&#37197;&#32622;beans,&#21487;&#20197;&#23884;&#22871;beans&#37197;&#32622;&#19981;&#21516;&#30340;&#29615;&#22659;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="&#x6fc0;&#x6d3b; profile" ID="ID_1661760770" CREATED="1509495662871" MODIFIED="1509495878795"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1. &#20316;&#20026;DispatcherServlet&#30340;&#21021;&#22987;&#21270;&#23646;&#24615;
+    </p>
+    <p>
+      2. &#20316;&#20026;Web&#24212;&#29992;&#30340;&#19978;&#19979;&#25991;&#21442;&#25968;
+    </p>
+    <p>
+      3. &#20316;&#20026;JNDI&#26465;&#30446;
+    </p>
+    <p>
+      4. &#20316;&#20026;&#29615;&#22659;&#21464;&#37327;
+    </p>
+    <p>
+      5. &#20316;&#20026;JVM&#31995;&#32479;&#23646;&#24615;
+    </p>
+    <p>
+      6. &#22312;&#38598;&#25104;&#27979;&#35797;&#31867;&#19978;&#20351;&#29992;@ActiveProfiles&#27880;&#35299;&#35774;&#32622;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="spring.profiles.active" ID="ID_102438691" CREATED="1509495686942" MODIFIED="1509495698267"/>
+<node TEXT="spring.profiles.default" ID="ID_26974779" CREATED="1509495698984" MODIFIED="1509495707703"/>
+</node>
+</node>
+<node TEXT="&#x6761;&#x4ef6;&#x5316;bean" ID="ID_708168493" CREATED="1510477877215" MODIFIED="1510478135126"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#26681;&#25454;&#26576;&#31181;&#26465;&#20214;&#21019;&#24314;bean&#65292;&#23545;&#20110;profile&#37197;&#32622;&#20854;&#23454;&#20063;&#26159;&#36825;&#31181;&#37197;&#32622;&#30340;&#29305;&#27530;&#19968;&#31181;&#65292;&#26465;&#20214;&#26159;&#29615;&#22659;&#37197;&#32622;&#12290;
+    </p>
+    <p>
+      &#22312;&#37197;&#32622;bean&#30340;&#26102;&#20505;&#20351;&#29992;@Condition(oneConditionClass)&#65292;&#20854;&#20013;oneConditionClass&#26159;Condition&#30340;&#26576;&#19968;&#20010;&#20010;&#24615;&#21270;&#23454;&#29616;&#12290;&#20027;&#35201;&#26159;&#23454;&#29616;&#20854;&#20013;&#30340;matches&#26041;&#27861;&#12290;
+    </p>
+  </body>
+</html>
 
 </richcontent>
+</node>
+<node TEXT="&#x81ea;&#x52a8;&#x914d;&#x7f6e;&#x7684;&#x6b67;&#x4e49;" ID="ID_1923858925" CREATED="1510478145859" MODIFIED="1510478166154">
+<node TEXT="&#x6807;&#x793a;&#x9996;&#x9009;bean" ID="ID_1894696759" CREATED="1510478176242" MODIFIED="1510478233097"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      @Primary
+    </p>
+    <p>
+      &lt;*** primary=&quot;true&quot; /&gt;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x9650;&#x5b9a;&#x81ea;&#x52a8;&#x88c5;&#x914d;&#x7684;bean" ID="ID_1331096028" CREATED="1510478234393" MODIFIED="1510478525884"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      @Qualifier;
+    </p>
+    <p>
+      &#22312;&#31867;&#19978;&#22768;&#26126;&#38480;&#23450;&#31526;(@Qualifier);
+    </p>
+    <p>
+      &#33258;&#23450;&#20041;&#38480;&#23450;&#31526;&#27880;&#35299;&#65292;&#25509;&#21475;&#29992;@Qualifier&#27880;&#35299;&#65292;&#27880;&#20837;&#30340;bean&#22312;&#31867;&#19978;&#20351;&#29992;&#27880;&#35299;&#12290;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Bean&#x7684;&#x4f5c;&#x7528;&#x57df;" FOLDED="true" ID="ID_141228446" CREATED="1510478530670" MODIFIED="1510478964278"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      web&#30340;&#22768;&#26126;&#33539;&#22260;&#30340;&#26102;&#20505;&#35201;&#35746;&#22909;&#27169;&#24335;&#65306;
+    </p>
+    <p>
+      &#20004;&#20010;web&#30340;&#33539;&#22260;&#29992;&#30340;&#19981;&#26159;ConfigurableFactory.SCOPE_***,&#32780;&#26159;WebApplicationContext.SCOPE.***;
+    </p>
+    <p>
+      proxyMode=ScopedProxyMode.INTERFACES&#26159;&#35813;&#31867;&#26159;&#19968;&#20010;&#25509;&#21475;&#31867;&#65307;
+    </p>
+    <p>
+      proxyMode=ScopePxoxyMode.TARGET_CLASS&#26159;&#35813;&#31867;&#26159;&#19968;&#20010;&#23454;&#20307;&#31867;&#12290;
+    </p>
+    <p>
+      &#22312;xml&#37197;&#32622;&#25991;&#20214;&#20013;&#65292;&#20351;&#29992;aop,&lt;aop:scoped-proxy/&gt;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Singleton&#x5355;&#x5217;" ID="ID_1465757680" CREATED="1510478556697" MODIFIED="1510478570697"/>
+<node TEXT="Prototype&#x539f;&#x578b;" ID="ID_343407466" CREATED="1510478571294" MODIFIED="1510478587246"/>
+<node TEXT="Session&#x4f1a;&#x8bdd;(web)" ID="ID_1682002794" CREATED="1510478587933" MODIFIED="1510478602068"/>
+<node TEXT="Request&#x8bf7;&#x6c42;(web)" ID="ID_1976801389" CREATED="1510478602946" MODIFIED="1510478621716"/>
+</node>
+<node TEXT="&#x8fd0;&#x884c;&#x65f6;&#x6ce8;&#x5165;" ID="ID_930024986" CREATED="1510478794957" MODIFIED="1510478986073">
+<node TEXT="&#x5c5e;&#x6027;&#x5360;&#x4f4d;&#x7b26;" ID="ID_478669394" CREATED="1510479104190" MODIFIED="1510479268222"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      @PropertySource&#24341;&#20837;&#23646;&#24615;&#25991;&#20214;&#65307;
+    </p>
+    <p>
+      T()&#26041;&#27861;&#26159;&#23558;&#38745;&#24577;&#31867;&#36716;&#21270;&#20026;&#23454;&#20307;&#31867;&#65292;&#26041;&#20415;&#23454;&#29992;&#38745;&#24577;&#26041;&#27861;&#65307;
+    </p>
+    <p>
+      ${property_name}&#24341;&#29992;&#23646;&#24615;&#20540;&#65307;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="SpEL" ID="ID_1833576827" CREATED="1510479125446" MODIFIED="1510479328424"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      #{SpEL}&#24341;&#29992;SpEL&#30340;&#20540;&#65307;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="&#x5b57;&#x9762;&#x503c;" ID="ID_1669419627" CREATED="1510479330455" MODIFIED="1510479358317"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#24847;&#20041;&#19981;&#22823;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x5f15;&#x7528;bean&#x3001;&#x5c5e;&#x6027;&#x548c;&#x65b9;&#x6cd5;" ID="ID_1287640900" CREATED="1510479359341" MODIFIED="1510479375394"/>
+<node TEXT="&#x8868;&#x8fbe;&#x5f0f;&#x4e2d;&#x4f7f;&#x7528;&#x7c7b;&#x578b;" ID="ID_1394195805" CREATED="1510479387380" MODIFIED="1510479407039"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      T()
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="SpEL&#x8fd0;&#x7b97;&#x7b26;" ID="ID_1362008091" CREATED="1510479407720" MODIFIED="1510479427522">
+<node TEXT="&#x4e00;&#x822c;&#x8fd0;&#x7b97;" ID="ID_1540381759" CREATED="1510479446207" MODIFIED="1510479450991"/>
+<node TEXT="&#x6b63;&#x5219;&#x8868;&#x8fbe;&#x5f0f;" ID="ID_90239511" CREATED="1510479451616" MODIFIED="1510479581637"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      matches
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x96c6;&#x5408;" ID="ID_741777442" CREATED="1510479460829" MODIFIED="1510479567201"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      .?[]&#36807;&#28388;&#65292;&#32467;&#26524;&#26159;&#38598;&#21512;&#65306;
+    </p>
+    <p>
+      .^[],.$[]&#31532;&#19968;&#20010;&#65292;&#26368;&#21518;&#19968;&#20010;&#21305;&#37197;&#39033;&#65307;
+    </p>
+    <p>
+      .![]&#25237;&#24433;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 </node>
